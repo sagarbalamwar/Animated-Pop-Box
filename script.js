1c9862beb7de818315624b7c252f768d36b12fb1
Sagar <sagarbalamwar888@gmail.com>
2022-07-21 15:02:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let togglebtn = document.querySelector('.togglebtn');
+let container = document.querySelector('.container');
+togglebtn.onclick = function(){
+  container.classList.toggle('active');
+}
